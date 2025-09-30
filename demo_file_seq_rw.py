2a065685881f7770dbe2aaa8a191f46aaf16b921
zubinpaulofficial <zubinpaulofficial@gmail.com>
2025-09-30 17:16:00 +0100

    Created demo_joy_of_sets.py

---
+++ demo_file_seq_rw.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# Author: Zubin Paul
+# Description: This script will demo
+"""
+    Docstring:
+"""
+movies = {'kymran': ['indiana jones', 'bridget jones', 'mamma mia'],
+        'charles': ['puss in boots', 'the last wish', 'django unchained'],
+        'diren': ['top gun', 'wolf of wall street', 'rio'],
+          'donald': ['lotr', 'the hobbit', 'star wars']
+}
+
+fh_out = open(r"/Users/zpl123/Documents/Pycharm Projects/Sky_SuperTech_ZP_2025/movies.txt", mode="wt")
+
+for name in movies.keys():
+    print(name, movies[name])
+    fh_out.write(f"{name} {movies[name]}\n")
+fh_out.close()
+
+print('-' * 60)
+
+#Opening in TEXT Mode (READING)
+fh_in = open(r"/Users/zpl123/Documents/Pycharm Projects/Sky_SuperTech_ZP_2025/movies.txt", mode="rt")
+
+# text = fh_in.read() #read entire file into a str object!
+text = fh_in.readline()
+print(text)
+
+fh_in.close()
