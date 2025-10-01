734a22bb3a02b2fa210e10ed8c54629ee7a704f1
zubinpaulofficial <zubinpaulofficial@gmail.com>
2025-10-01 14:52:15 +0100

    Create adv.py module with mod, pow, and sqrt functions

---
+++ calculator/app/adv.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# Author: Zubin Paul
+# Description: This module defines advanced calc functions
+"""
+    Advanced Calc Module with mod, power and sqrt functions
+"""
+
+def mod(x, z):
+    """ Return REMAINDER after integer div of x by z """
+    return float(x%z)
+
+def power(x, z):
+    """ Return the POWER of x to z as a float """
+    return float(x**z)
+
+def sqrt(x):
+    """ Return Square root of x to 3 decimal places """
+    return round(x**0.5, 3)
+
+print("########### Adv Calc Examples ############")
+print(f"4 % 3 = {mod(4, 3)}")
+print(f"4 ** 3 = {power(4, 3)}")
+print(f"\N{square root}99 = {sqrt(99)}")
+print("###########################################")
