af55d7873a5c1ea6fb5a8039f0fddbb0162a3c59
zubinpaulofficial <zubinpaulofficial@gmail.com>
2025-10-01 14:46:04 +0100

    Create basic.py module with add, mul, and div functions

---
+++ calculator/app/basic.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# Author: Zubin Paul
+# Description: This script is a VERY Basic Calculator App with few functions!
+# Comments for the developer/coder
+"""
+    Basic Calculator App with add, multiply and divide features
+"""
+import sys
+
+def add(*args):
+    """ Return SUM of all arguments as a float """
+    sum = 0
+    for num in args:
+        sum += num
+    return float(sum)
+
+def mul(*args):
+    """ Return PRODUCT of all arguments as a float """
+    total = 1
+    for num in args:
+        total *= num
+    return float(total)
+
+def div(x, z):
+    """ Return QUOTIENT of x divided by z to 3 decimal places """
+    return round(x/z, 3)
+
+print("----------- Basic Calc Examples ------------")
+print(f"4 + 3 + 2 + 1 = {add(4, 3, 2, 1)}")
+print(f"4 * 3 * 2 = {mul(4, 3, 2)}")
+print(f"4 / 3 = {div(4, 3)}")
+print("--------------------------------------------")
+
+sys.exit(0)
